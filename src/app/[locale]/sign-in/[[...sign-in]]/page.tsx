a5a071758b7e49718ab5282459d73dedcbcd3751
kevinnkansah <kevinnkansah6@gmail.com>
2025-02-08 21:24:11 +0200

    revert clerk

--- src/app/[locale]/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { SignIn } from "@clerk/nextjs"
+import { Card } from "@nextui-org/react"
+import * as Clerk from '@clerk/elements/common'
+import * as SignIn from '@clerk/elements/sign-in'
 import { useTranslations } from 'next-intl'
-import { Flex } from '@/once-ui/components'
+import { Button, Flex, Text } from '@/once-ui/components'
 
 export default function SignInPage() {
   const t = useTranslations('signin')
@@ -13,25 +15,59 @@ export default function SignInPage() {
       fillHeight
       alignItems="center"
       justifyContent="center"
+      style={{ background: 'var(--color-brand-weak)' }}
     >
-      <SignIn
-        appearance={{
-          variables: {
-            colorPrimary: '#14b8a6',
-            colorTextOnPrimaryBackground: 'white',
-            borderRadius: '0.5rem'
-          },
-          elements: {
-            socialButtonsBlockButton: {
-              backgroundColor: '#0d9488',
-              border: '1px solid #0f766e',
-              '&:hover': {
-                backgroundColor: '#0f766e',
-              }
-            },
-          }
-        }}
-      />
+      <SignIn.Root>
+        <SignIn.Step name="start">
+          <Flex
+            style={{ overflow: 'hidden' }}
+            position="relative"
+            fillWidth
+            padding="xl"
+            radius="l"
+            direction="column"
+            alignItems="center"
+            background="surface"
+            border="neutral-medium"
+            borderStyle="solid"
+            maxWidth={480}
+          >
+            <Text
+              variant="heading-strong-l"
+              marginBottom="l"
+              align="center"
+            >
+              {t('description')}
+            </Text>
+
+            <Flex direction="column" gap="8" fillWidth>
+              <Clerk.GlobalError className="text-sm text-error" />
+
+              <Flex justifyContent="center" marginBottom="8">
+                <Clerk.Connection name="google">
+                  <Button variant="primary" prefixIcon="google" size="l">
+                    Continue with Google
+                  </Button>
+                </Clerk.Connection>
+              </Flex>
+
+              <Text
+                variant="body-default-s"
+                color="neutral-faded"
+                align="center"
+              >
+                No account?{' '}
+                <Clerk.Link
+                  navigate="sign-up"
+                  className="font-medium text-primary hover:underline focus:underline"
+                >
+                  Sign up
+                </Clerk.Link>
+              </Text>
+            </Flex>
+          </Flex>
+        </SignIn.Step>
+      </SignIn.Root>
     </Flex>
   )
 }
--- src/app/[locale]/sign-up/[[...sign-up]]/page.tsx
+++ src/app/[locale]/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { SignUp } from "@clerk/nextjs"
-import { dark } from "@clerk/themes"
+import * as Clerk from '@clerk/elements/common'
+import * as SignUp from '@clerk/elements/sign-up'
 import { useTranslations } from 'next-intl'
-import { Flex } from '@/once-ui/components'
+import { Button, Flex, Text } from '@/once-ui/components'
 
 export default function SignUpPage() {
   const t = useTranslations('signup')
@@ -14,28 +14,59 @@ export default function SignUpPage() {
       fillHeight
       alignItems="center"
       justifyContent="center"
-
+      style={{ background: 'var(--color-brand-weak)' }}
     >
-      <SignUp
-        appearance={{
-          baseTheme: dark,
-          variables: {
-            colorPrimary: '#14b8a6',
-            colorTextOnPrimaryBackground: 'white',
-            borderRadius: '0.5rem'
-          },
-          elements: {
-            socialButtonsBlockButton: {
-              backgroundColor: '#0d9488',
-              border: '1px solid #0f766e',
-              '&:hover': {
-                backgroundColor: '#0f766e',
-              }
-            },
-            
-          }
-        }}
-      />
+      <SignUp.Root>
+        <SignUp.Step name="start">
+          <Flex
+            style={{ overflow: 'hidden' }}
+            position="relative"
+            fillWidth
+            padding="xl"
+            radius="l"
+            direction="column"
+            alignItems="center"
+            background="surface"
+            border="neutral-medium"
+            borderStyle="solid"
+            maxWidth={480}
+          >
+            <Text
+              variant="heading-strong-l"
+              marginBottom="l"
+              align="center"
+            >
+              {t('description')}
+            </Text>
+
+            <Flex direction="column" gap="8" fillWidth>
+              <Clerk.GlobalError className="text-sm text-error" />
+
+              <Flex justifyContent="center" marginBottom="8">
+                <Clerk.Connection name="google">
+                  <Button variant="primary" prefixIcon="google" size="l">
+                    Continue with Google
+                  </Button>
+                </Clerk.Connection>
+              </Flex>
+
+              <Text
+                variant="body-default-s"
+                color="neutral-faded"
+                align="center"
+              >
+                Already have an account?{' '}
+                <Clerk.Link
+                  navigate="sign-in"
+                  className="font-medium text-primary hover:underline focus:underline"
+                >
+                  Sign in
+                </Clerk.Link>
+              </Text>
+            </Flex>
+          </Flex>
+        </SignUp.Step>
+      </SignUp.Root>
     </Flex>
   )
 }
